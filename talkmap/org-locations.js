8d197443eb4745e8f95e8fea877161ef047089f0
Kishan Bellur <kishanbellur@gmail.com>
2020-05-08 14:50:27 -0400

    update talks v4

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,18 @@
 var addressPoints = [
   [
+    "ACS UPLS Student Research Symposium | Marquette, MI, USA",
+    46.4481521,
+    -87.6305899
+  ],
+  [
     "Graduate Research Colloquium, Michigan Tech | Houghton, MI, USA",
     46.8591287,
     -88.7774734
+  ],
+  [
+    "ASME-IMECE | Houston, TX, USA",
+    29.7589382,
+    -95.3676974
   ],
   [
     "26th Space Cryogenics Workshop | Phoenix, AZ, USA",
@@ -10,13 +20,28 @@ var addressPoints = [
     -112.0741417
   ],
   [
+    "69th Annual Meeting of the APS Division of Fluid Dynamics | Portland, OR, USA",
+    45.5202471,
+    -122.6741949
+  ],
+  [
+    "The First Thermal and Fluids Engineering Summer Conference | New York, NY, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
     "ASME-IMECE | Montreal, ON, Canada",
     43.667986299999995,
     -79.373456427228
   ],
   [
-    "UC-Berkeley Institute for Testing Science | Berkeley CA, USA",
-    37.8708393,
-    -122.2728639
+    "27th Space Cryogenics Workshop | Chicago, IL, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    " ASME Summer Heat Transfer Conference | Bellevue, WA , USA",
+    47.6144219,
+    -122.1923372
   ]
 ];
