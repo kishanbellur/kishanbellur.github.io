91346566377df537a05d12dc902c3e4461997bce
Kishan Bellur <kishanbellur@gmail.com>
2020-05-31 10:09:28 -0400

    may31: updating talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,6 +5,31 @@ var addressPoints = [
     -87.6305899
   ],
   [
+    "ASME Summer Heat Transfer Conference, AIChE Symposium in Honor of Prof. Peter C. Wayner, Jr. II | Bellevue, WA, USA",
+    47.6144219,
+    -122.1923372
+  ],
+  [
+    "35th Annual Meeting of the American Society for Gravitational and Space Research | Denver, CO, USA",
+    39.7392364,
+    -104.9848623
+  ],
+  [
+    "Gordon Research Conference on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
+    44.0454286,
+    10.4792677
+  ],
+  [
+    "33rd Annual Meeting of the American Society for Gravitational and Space Research | Renton, WA, USA",
+    47.4799078,
+    -122.2034496
+  ],
+  [
+    "34th Annual Meeting of the American Society for Gravitational and Space Research, Washington, | Washington, DC, USA",
+    38.8949855,
+    -77.0365708
+  ],
+  [
     "Graduate Research Colloquium, Michigan Tech | Houghton, MI, USA",
     46.8591287,
     -88.7774734
@@ -15,9 +40,19 @@ var addressPoints = [
     -95.3676974
   ],
   [
-    "26th Space Cryogenics Workshop | Phoenix, AZ, USA",
-    33.4484367,
-    -112.0741417
+    "Portage Lake District Library | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
+  ],
+  [
+    "Portland State University | Portland, OR, USA",
+    45.5202471,
+    -122.6741949
+  ],
+  [
+    "Material Science and Engineering Seminar, Michigan Tech. | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
   ],
   [
     "69th Annual Meeting of the APS Division of Fluid Dynamics | Portland, OR, USA",
@@ -30,9 +65,29 @@ var addressPoints = [
     -74.0060152
   ],
   [
+    "11th World Conference on Neutron Radiography | Sydney, Australia",
+    -33.8548157,
+    151.2164539
+  ],
+  [
     "ASME-IMECE | Montreal, ON, Canada",
     43.667986299999995,
     -79.373456427228
+  ],
+  [
+    "Vijaya College, Bangalore University, | Bangalore, India",
+    12.9791198,
+    77.5912997
+  ],
+  [
+    "26th Space Cryogenics Workshop | Phoenix, AZ, USA",
+    33.4484367,
+    -112.0741417
+  ],
+  [
+    "Gordon Research Seminar on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
+    44.0454286,
+    10.4792677
   ],
   [
     "27th Space Cryogenics Workshop | Chicago, IL, USA",
@@ -40,7 +95,7 @@ var addressPoints = [
     -87.6244212
   ],
   [
-    " ASME Summer Heat Transfer Conference | Bellevue, WA , USA",
+    "ASME Summer Heat Transfer Conference | Bellevue, WA , USA",
     47.6144219,
     -122.1923372
   ]
