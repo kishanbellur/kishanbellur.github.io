55387a323e4fe8465dcc659217f4664380fba4c4
kishanbellur <64089759+kishanbellur@users.noreply.github.com>
2023-10-03 16:48:13 -0700

    talks and talkmap updated

--- _talks/20-ASGSR.md
+++
@@ -1,9 +0,0 @@
----
-title: "Testing fundamental assumptions using Constained Vapor Bubble (CVB) data from ISS experiments "
-collection: talks
-type: "Oral presentation"
-permalink: /talks/11/29/20-ASGSR
-venue: "36th Annual Meeting of the American Society for Gravitational and Space Research"
-date: 11/29/20
-location: "Virtual"
----
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,10 +1,5 @@
 var addressPoints = [
   [
-    "33rd Annual Meeting of the American Society for Gravitational and Space Research | Renton, WA, USA",
-    47.4799078,
-    -122.2034496
-  ],
-  [
     "Institute for Mathematics and its Applications (IMA), University of Minnesota | Minneapolis, MN, USA",
     44.9772995,
     -93.2654692
@@ -25,49 +20,14 @@ var addressPoints = [
     -88.7774734
   ],
   [
-    "38th Annual Meeting of the American Society for Gravitational and Space Research | Houston, TX, USA",
-    29.7589382,
-    -95.3676974
-  ],
-  [
     "29th Space Cryogenics Workshop | Virtual",
     39.2171138,
     -9.278711124684799
   ],
   [
-    "ASME-IMECE | Houston, TX, USA",
-    29.7589382,
-    -95.3676974
-  ],
-  [
-    "Thermal and Fluids Analysis Workshop (TFAWS)  | College Park, MD, USA",
-    38.980666,
-    -76.9369189
-  ],
-  [
-    "Gordon Research Conference on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
-    44.0826068,
-    10.4638738
-  ],
-  [
-    "Graduate Research Colloquium, Michigan Tech | Houghton, MI, USA",
-    46.8591287,
-    -88.7774734
-  ],
-  [
     "34th Annual Meeting of the American Society for Gravitational and Space Research, Washington, | Washington, DC, USA",
     38.8950368,
     -77.0365427
-  ],
-  [
-    "ASME Summer Heat Transfer Conference, AIChE Symposium in Honor of Prof. Peter C. Wayner, Jr. II | Bellevue, WA, USA",
-    47.6144219,
-    -122.192337
-  ],
-  [
-    "8th Thermal and Fluids Engineering Conference | College Park, MD, USA",
-    38.980666,
-    -76.9369189
   ],
   [
     "Portland State University | Portland, OR, USA",
@@ -85,9 +45,9 @@ var addressPoints = [
     -87.6305899
   ],
   [
-    "69th Annual Meeting of the APS Division of Fluid Dynamics | Portland, OR, USA",
-    45.5202471,
-    -122.674194
+    "Gordon Research Conference on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
+    44.0826068,
+    10.4638738
   ],
   [
     "11th World Conference on Neutron Radiography | Sydney, Australia",
@@ -95,9 +55,9 @@ var addressPoints = [
     151.2082848
   ],
   [
-    "ASME Summer Heat Transfer Conference | Washington, DC, USA",
-    38.8950368,
-    -77.0365427
+    "ASME Summer Heat Transfer Conference, AIChE Symposium in Honor of Prof. Peter C. Wayner, Jr. II | Bellevue, WA, USA",
+    47.6144219,
+    -122.192337
   ],
   [
     "35th Annual Meeting of the American Society for Gravitational and Space Research | Denver, CO, USA",
@@ -110,9 +70,54 @@ var addressPoints = [
     10.4638738
   ],
   [
+    "Graduate Research Colloquium, Michigan Tech | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
+  ],
+  [
+    "69th Annual Meeting of the APS Division of Fluid Dynamics | Portland, OR, USA",
+    45.5202471,
+    -122.674194
+  ],
+  [
+    "38th Annual Meeting of the American Society for Gravitational and Space Research | Houston, TX, USA",
+    29.7589382,
+    -95.3676974
+  ],
+  [
     "27th Space Cryogenics Workshop | Chicago, IL, USA",
     41.8755616,
     -87.6244212
+  ],
+  [
+    "36th Annual Meeting of the American Society for Gravitational and Space Research | Virtual",
+    39.2171138,
+    -9.278711124684799
+  ],
+  [
+    "NASA Glenn Research Center | Cleveland, OH, USA",
+    41.4996574,
+    -81.6936772
+  ],
+  [
+    "33rd Annual Meeting of the American Society for Gravitational and Space Research | Renton, WA, USA",
+    47.4799078,
+    -122.2034496
+  ],
+  [
+    "ASME Summer Heat Transfer Conference | Washington, DC, USA",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "University of Cincinnati | Cincinnati, OH, USA",
+    39.1014537,
+    -84.5124602
+  ],
+  [
+    "ASME-IMECE | Houston, TX, USA",
+    29.7589382,
+    -95.3676974
   ],
   [
     "The First Thermal and Fluids Engineering Summer Conference | New York, NY, USA",
@@ -125,9 +130,14 @@ var addressPoints = [
     -112.074141
   ],
   [
-    "36th Annual Meeting of the American Society for Gravitational and Space Research | Virtual",
-    39.2171138,
-    -9.278711124684799
+    "Vijaya College, Bangalore University | Bangalore, India",
+    12.9767936,
+    77.590082
+  ],
+  [
+    "ASME-IMECE | Montreal, ON, Canada",
+    47.2477017,
+    -84.5964062
   ],
   [
     "Portage Lake District Library | Houghton, MI, USA",
@@ -135,23 +145,13 @@ var addressPoints = [
     -88.7774734
   ],
   [
-    "Vijaya College, Bangalore University | Bangalore, India",
-    12.9767936,
-    77.590082
+    "Thermal and Fluids Analysis Workshop (TFAWS)  | College Park, MD, USA",
+    38.980666,
+    -76.9369189
   ],
   [
-    "NASA Glenn Research Center | Cleveland, OH, USA",
-    41.4996574,
-    -81.6936772
-  ],
-  [
-    "University of Cincinnati | Cincinnati, OH, USA",
-    39.1014537,
-    -84.5124602
-  ],
-  [
-    "ASME-IMECE | Montreal, ON, Canada",
-    47.2477017,
-    -84.5964062
+    "8th Thermal and Fluids Engineering Conference | College Park, MD, USA",
+    38.980666,
+    -76.9369189
   ]
 ];
