3f5d93563e0dac574317a3dd8defa8cfa11f7cf1
kishanbellur <64089759+kishanbellur@users.noreply.github.com>
2025-01-04 13:12:59 +0530

    update talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,167 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Institute for Mathematics and its Applications (IMA), University of Minnesota | Minneapolis, MN, USA",
+    44.9772995,
+    -93.2654692
+  ],
+  [
+    "ASME Summer Heat Transfer Conference | Bellevue, WA , USA",
+    47.6144219,
+    -122.192337
+  ],
+  [
+    "75th Annual Meeting of the American Physical Society - Division of Fluid Dynamics (APS DFD) | Indianapolis, IN, USA",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "75th Annual Meeting of the American Physical Society \u00d0 Division of Fluid Dynamics (APS DFD) | Indianapolis, IN, USA",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Material Science and Engineering Seminar, Michigan Tech. | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
+  ],
+  [
+    "29th Space Cryogenics Workshop | Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "34th Annual Meeting of the American Society for Gravitational and Space Research, Washington, | Washington, DC, USA",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "Portland State University | Portland, OR, USA",
+    45.5202471,
+    -122.674194
+  ],
+  [
+    "Surface Wettability Effects on Phase Change (SWEP) workshop | Brighton, UK",
+    50.8214626,
+    -0.1400561
+  ],
+  [
+    "ACS UPLS Student Research Symposium | Marquette, MI, USA",
+    46.4481521,
+    -87.6305899
+  ],
+  [
+    "Gordon Research Conference on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
+    44.0754957,
+    10.4821594
+  ],
+  [
+    "11th World Conference on Neutron Radiography | Sydney, Australia",
+    -33.8698439,
+    151.2082848
+  ],
+  [
+    "ASME Summer Heat Transfer Conference, AIChE Symposium in Honor of Prof. Peter C. Wayner, Jr. II | Bellevue, WA, USA",
+    47.6144219,
+    -122.192337
+  ],
+  [
+    "35th Annual Meeting of the American Society for Gravitational and Space Research | Denver, CO, USA",
+    39.7392364,
+    -104.984862
+  ],
+  [
+    "Gordon Research Seminar on Micro and Nanoscale Phase Change Heat Transfer | Lucca (Barga), Italy",
+    44.0754957,
+    10.4821594
+  ],
+  [
+    "Graduate Research Colloquium, Michigan Tech | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
+  ],
+  [
+    "69th Annual Meeting of the APS Division of Fluid Dynamics | Portland, OR, USA",
+    45.5202471,
+    -122.674194
+  ],
+  [
+    "38th Annual Meeting of the American Society for Gravitational and Space Research | Houston, TX, USA",
+    29.7589382,
+    -95.3676974
+  ],
+  [
+    "27th Space Cryogenics Workshop | Chicago, IL, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "36th Annual Meeting of the American Society for Gravitational and Space Research | Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "NASA Glenn Research Center | Cleveland, OH, USA",
+    41.4996574,
+    -81.6936772
+  ],
+  [
+    "33rd Annual Meeting of the American Society for Gravitational and Space Research | Renton, WA, USA",
+    47.4799078,
+    -122.2034496
+  ],
+  [
+    "ASME Summer Heat Transfer Conference | Washington, DC, USA",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "University of Cincinnati | Cincinnati, OH, USA",
+    39.1014537,
+    -84.5124602
+  ],
+  [
+    "ASME-IMECE | Houston, TX, USA",
+    29.7589382,
+    -95.3676974
+  ],
+  [
+    "The First Thermal and Fluids Engineering Summer Conference | New York, NY, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "26th Space Cryogenics Workshop | Phoenix, AZ, USA",
+    33.4484367,
+    -112.074141
+  ],
+  [
+    "Vijaya College, Bangalore University | Bangalore, India",
+    12.98815675,
+    77.62260003796
+  ],
+  [
+    "ASME-IMECE | Montreal, Quebec, Canada",
+    45.5031824,
+    -73.5698065
+  ],
+  [
+    "Portage Lake District Library | Houghton, MI, USA",
+    46.8591287,
+    -88.7774734
+  ],
+  [
+    "Thermal and Fluids Analysis Workshop (TFAWS)  | College Park, MD, USA",
+    38.980666,
+    -76.9369189
+  ],
+  [
+    "8th Thermal and Fluids Engineering Conference | College Park, MD, USA",
+    38.980666,
+    -76.9369189
+  ],
+  [
+    "American Physical Society - Division of Fluid Dynamics | Washington, DC, USA",
+    38.8950368,
+    -77.0365427
+  ]
+];
